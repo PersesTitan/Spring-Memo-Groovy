9329c92345a67836e75bedd23db71accf9212831
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-20 02:21:52 +0900

    Initial commit

---
+++ src/main/groovy/com/spring/MemoGroovy/MemoGroovyApplication.groovy
@@ -0,0 +1,13 @@
+package com.spring.MemoGroovy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MemoGroovyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MemoGroovyApplication, args)
+	}
+
+}
