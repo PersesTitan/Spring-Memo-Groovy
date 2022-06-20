92986504c3711cc37c784002341209ae91bb3b51
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-20 14:12:40 +0900

    메모 저장소 생성

---
+++ src/main/groovy/com/spring/MemoGroovy/repository/MemoRepository.groovy
@@ -0,0 +1,43 @@
+package com.spring.MemoGroovy.repository
+
+import com.spring.MemoGroovy.domain.Memo
+import lombok.RequiredArgsConstructor
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Repository
+
+import javax.persistence.EntityManager
+
+@Repository
+class MemoRepository {
+
+    @Autowired
+    private EntityManager entityManager
+
+    def save(Memo memo) {
+        entityManager.persist memo
+    }
+
+    def remove(Memo memo) {
+        entityManager.remove memo
+    }
+
+    def update(Memo memo, String title, String content) {
+        memo.setTitle title
+        memo.setContent content
+    }
+
+    def findOne(Long id) {
+        return entityManager.find(Memo.class, id)
+    }
+
+    def findAll() {
+        return entityManager.createQuery("SELECT M FROM Memo AS M", Memo.class)
+                .getResultList()
+    }
+
+    def findSearch(String keyWord) {
+        return entityManager.createQuery("SELECT M FROM Memo AS M WHERE M.title LIKE :keyWord")
+                .setParameter("keyWord", "%${keyWord}%")
+                .getResultList()
+    }
+}
