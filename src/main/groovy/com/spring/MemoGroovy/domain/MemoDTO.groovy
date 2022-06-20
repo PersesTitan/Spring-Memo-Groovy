d03d91e320f13d8b58e523c96a27e97002c9fadc
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-20 14:12:27 +0900

    메모 DTO 생성

---
+++ src/main/groovy/com/spring/MemoGroovy/domain/MemoDTO.groovy
@@ -0,0 +1,12 @@
+package com.spring.MemoGroovy.domain
+
+class MemoDTO {
+
+    String title
+    String content
+
+    MemoDTO(String title, String content) {
+        this.title = title
+        this.content = content
+    }
+}
